6a836a8429f93ce17e0496f6df1734e5b1a9851e
Kevin <kevibk@gmail.com>
2022-02-18 17:50:26 +0530

    Completed Module 2 fix

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
 @RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 
